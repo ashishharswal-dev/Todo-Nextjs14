1f5df910c5df528dff0fe900d47b4b2111726b47
ashishharswal-dev <113756439+ashishharswal-dev@users.noreply.github.com>
2024-01-19 13:28:27 +0530

    Add files via upload
    first commit

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+import AddTodo from "@/components/shared/AddTodo"
+import Todo from "@/components/shared/Todo";
+import { prisma } from "@/utils/prisma"
+
+async function getData() {
+  const data = await prisma.todo.findMany({
+    select: {
+      id: true,
+      title: true,
+      isCompleted: true
+    },
+    orderBy: {
+      createdAt: 'desc',
+    }
+  })
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+  return (
+    <div className="w-screen py-20 flex flex-col items-center">
+      <span className="text-3xl font-extrabold uppercase">todo app</span>
+      <h1 className="text-3xl font-extrabold uppercase mb-5">Next.js 14
+        <span className="text-orange-700 ml-2">Server Actions</span>
+      </h1>
+      <div className="flex justify-center flex-col items-center w-[1000px]">
+        <AddTodo />
+        <div className="flex flex-col gap-5 items-center justify-center mt-10 w-full">
+          {
+            data.map((todo,id)=>(
+              <div className="w-full" key={id}>
+                <Todo todo={todo} />
+              </div>
+            ))
+          }
+        </div>
+      </div>
+
+    </div>
+  )
+}
